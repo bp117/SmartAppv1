4d3944db208a2cae4d8f10859c39accc5bf56630
bp117 <bindupriya117@gmail.com>
2023-12-06 19:54:37 +0530

    Update GroupsContext.tsx

--- GroupsContext.tsx
+++ GroupsContext.tsx
@@ -1,21 +1,47 @@
+// GroupsContext.tsx
 import React, { createContext, useState } from 'react';
 import { Group } from './types';
 
 interface GroupsContextProps {
   groups: Group[];
-  setGroups: React.Dispatch<React.SetStateAction<Group[]>>;
+  addGroup: (group: Group) => void;
+  updateGroup: (group: Group) => void;
+  deleteGroup: (groupId: string) => void;
 }
 
+const initialState: Group[] = JSON.parse(localStorage.getItem('groups') || '[]');
+
 export const GroupsContext = createContext<GroupsContextProps>({
-  groups: [],
-  setGroups: () => {},
+  groups: initialState,
+  addGroup: () => {},
+  updateGroup: () => {},
+  deleteGroup: () => {}
 });
 
 export const GroupsProvider: React.FC = ({ children }) => {
-  const [groups, setGroups] = useState<Group[]>([]);
+  const [groups, setGroups] = useState<Group[]>(initialState);
+
+  const saveGroups = (groups: Group[]) => {
+    localStorage.setItem('groups', JSON.stringify(groups));
+    setGroups(groups);
+  };
+
+  const addGroup = (newGroup: Group) => {
+    saveGroups([...groups, newGroup]);
+  };
+
+  const updateGroup = (updatedGroup: Group) => {
+    const updatedGroups = groups.map(group => group.id === updatedGroup.id ? updatedGroup : group);
+    saveGroups(updatedGroups);
+  };
+
+  const deleteGroup = (groupId: string) => {
+    const updatedGroups = groups.filter(group => group.id !== groupId);
+    saveGroups(updatedGroups);
+  };
 
   return (
-    <GroupsContext.Provider value={{ groups, setGroups }}>
+    <GroupsContext.Provider value={{ groups, addGroup, updateGroup, deleteGroup }}>
       {children}
     </GroupsContext.Provider>
   );
